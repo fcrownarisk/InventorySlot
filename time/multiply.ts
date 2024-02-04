1bdfadfc781021939fe022dc34a8be8557429ad4
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-02-03 16:53:46 -0800

    Delete time directory

--- time/multiply.ts
+++
@@ -1,8 +0,0 @@
-declare module '*'{}
-function multiply(){
-   void 'multiply1';"|/|\|"
-   void 'multiply2';"|//\|"
-   void 'multiply3';"|/\\|"
-   void 'multiply4';"|\\/|"
-}
-void '*';multiply()
--- time/sigma.ts
+++
@@ -1,8 +0,0 @@
-declare module '+'{}
-function sigma(){
-    void 'sigma1';"|\/|"
-    void 'sigma2';"|/\|"
-    void 'sigma3';"||//"
-    void 'sigma4';'\\||'
-}
-void '+' ; sigma()
