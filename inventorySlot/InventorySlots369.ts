ece2a1825d9b3a36217d8de7933cefa7d7c09f2b
pipboy <155938693+fcrownarisk@users.noreply.github.com>
2024-02-08 15:19:09 +0800

    Add files via upload

--- inventorySlot/InventorySlots369.ts
+++ inventorySlot/InventorySlots369.ts
@@ -1,49 +1,35 @@
-import 'window/123456789.ts'
-import 'window/987654321.ts'
-type InventorySlots=(t: string, z: string, y: string, x: string) => void
-function InventorySlots1
-(t1:string,z1:string,y1:string,x1:string){
-type x1=("?? ?? ?? ??")
-type y1=("$$ $$ $$ $$")
-type z1=("!! !! !! !!")
-type t1=("@@ @@ @@ @@")
-do "(x1/y1/z1)%t1"
-while(false)
+type InventorySlots = (t: string, z: string, y: string, x: string) => void;
+const InventorySlots1: InventorySlots = (t1, z1, y1, x1) => {
+    const x1Items = "?? ?? ?? ??";
+    const y1Items = "$$ $$ $$ $$";
+    const z1Items = "!! !! !! !!";
+    const t1Items = "@@ @@ @@ @@";
+
+    console.log(`${x1Items}/${y1Items}/${z1Items}/${t1Items}`);
+};
+const InventorySlots2: InventorySlots = (t2, z2, y2, x2) => {
+    const x2Items = "-- -- -- --";
+    const y2Items = "|| || || ||";
+    const z2Items = "~~ ~~ ~~ ~~";
+    const t2Items = "\\ \\ \\ \\";
+    console.log(`${x2Items}/${y2Items}/${z2Items}/${t2Items}`);
+};
+const InventorySlots3: InventorySlots = (z1, z2, z3, z4) => {
+    const z1Items = "()() ()() ()() ()()";
+    const z2Items = "[][] [][] [][] [][]";
+    const z3Items = "{}{} {}{} {}{} {}{}";
+    const z4Items = "<><> <><> <><> <><>";
+
+    console.log(`${z1Items}/${z2Items}/${z3Items}/${z4Items}`);
+};
+const InventorySlots4: InventorySlots = (zz1, zz2, zz3, zz4) => {
+
+    const zz1Items = "++ ++ ++ ++";
+    const zz2Items = "-- -- -- --";
+    const zz3Items = "** ** ** **";
+    const zz4Items = "%% %% %% %%";
+
+
+    console.log(`${zz1Items}/${zz2Items}/${zz3Items}/${zz4Items}`);
 }
-function InventorySlots2
-(t2:string,z2:string,y2:string,x2:string){
-type x2=("-- -- -- --")
-type y2=("|| || || ||")
-type z2=("~~ ~~ ~~ ~~")
-type t2=("\\ \\ \\ \\")
-do "(x2/y2/z2)%t2"
-while(false)
-}
-function InventorySlots3
-(z1:string,z2:string,z3:string,z4:string){   
-z1=("()() ()() ()() ()()")
-z2=("[][] [][] [][] [][]")
-z3=("{}{} {}{} {}{} {}{}")
-z4=("<><> <><> <><> <><>")
-do "z1/z2/z3/z4"
-while(false)
-}
-function InventorySlots4
-(zz1:string,zz2:string,zz3:string,zz4:string){
-zz1=("++ ++ ++ ++")
-zz2=("-- -- -- --")
-zz3=("** ** ** **")
-zz4=("%% %% %% %%")
-do "zz1/zz2/zz3/zz4"   
-while(false)
-}  
-function InventorySlots5
-(t1:string,t2:string,t3:string,t4:string,t5:string){
-t1=("\\ \\ \\ \\")
-t2=("|| || \\ \\")
-t5=("|||||||||||")
-t3=("// // || ||")
-t4=("// // // //")
-do "(t1/t2/t3/t4)%t5"
-while(false)
-}
+
